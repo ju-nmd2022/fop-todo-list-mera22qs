60e80852ef52064af49667f72e1fc3ca1edfed6c
mera22qs <118453987+mera22qs@users.noreply.github.com>
2023-04-09 10:34:01 +0200

    console.log

--- mech.js
+++ mech.js
@@ -1,4 +1,4 @@
 document.getElementById("add").onclick = function () {
-  let bulletPoint = document.getElementsById("text-insert").value;
+  let bulletPoint = document.getElementById("text-insert").value;
   console.log("Hello", bulletPoint);
 };
