122bae89a7646f4bc3df3e6acfab029c2a2f6cdf
mera22qs <118453987+mera22qs@users.noreply.github.com>
2023-04-11 22:43:55 +0200

    basic css

--- mech.js
+++ mech.js
@@ -1,4 +1,4 @@
-const textIn = document.getElementById("text-insert");
+/*const textIn = document.getElementById("text-insert");
 const addbtn = document.getElementById("add");
 const textOut = document.getElementById("text-out");
 
@@ -6,4 +6,24 @@ function listItem() {
   textOut.innerHTML = textIn.value;
 }
 
-addbtn.addEventListener("click", listItem);
+addbtn.addEventListener("click", listItem);*/
+
+const form = document.getElementById("todo-input");
+const taskInput = document.getElementById("task-input");
+
+//array 
+
+let todos = [];
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  
+  saveTodo();
+});
+
+function saveTodo(){
+  const todoValue = taskInput.value
+
+  const todo 
+}
+
