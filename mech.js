b0c469e374069b5439ee0dbd37943ce1222c9d79
mera22qs <118453987+mera22qs@users.noreply.github.com>
2023-04-12 11:28:02 +0200

    remove items

--- mech.js
+++ mech.js
@@ -1,4 +1,4 @@
-// all lines of code was created with the help of https://youtu.be/-pRg_daFjfk by CodeDuck, aswell as the tutorial videos by Garrit
+// all lines of code was created with the help of https://youtu.be/-pRg_daFjfk by CodeDuck, as well as the tutorial videos by Garrit
 
 // The container that will show the user-input
 let bucketList = document.getElementById("paper");
@@ -15,4 +15,10 @@ function addToList() {
   let par = document.createElement("p");
   par.innerText = listItem.value;
   bucketList.appendChild(par);
+
+  // remove button
+  par.addEventListener("click", function () {
+    par.style.textDecoration;
+    bucketList.removeChild(par);
+  });
 }
