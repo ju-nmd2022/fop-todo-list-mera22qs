1cf5c7fdf4d7c0150bc5305f1cc05d06414395ae
mera22qs <118453987+mera22qs@users.noreply.github.com>
2024-01-05 00:42:07 +0100

    local storage

--- mech.js
+++ mech.js
@@ -1,4 +1,5 @@
-// the lines 14-22 of code was created with the help of https://youtu.be/-pRg_daFjfk by CodeDuck, https://youtu.be/Ttf3CEsEwMQ by developedbyed, as well as the tutorial videos by Garrit.
+
+//The code was created with the help of videos provided by Garrit Schaap and https://www.youtube.com/watch?v=-pRg_daFjfk&t=47s by CodeDuc. ChatGTP was also used to correct errors. 
 
 // The container that will show the user-input
 let bucketList = document.getElementById("paper");
@@ -16,22 +17,22 @@ function addToList() {
   let par = document.createElement("p");
   par.innerText = listItem.value;
   bucketList.appendChild(par);
+
+  //With the help of chat gtp, the inputs of the to-fo lists are converted into keys so they can be stored in local storage. 
+  // save to local storage with a consistent key format
+  localStorage.setItem("task_" + par.innerText, par.innerText);
   
-  //save to local storage 
-  localStorage.setItem(listItem.value);
-
-} 
-
-// add a line when the task is done
+  // add a line when the task is done
   par.addEventListener("click", function () {
     par.style.textDecoration = "line-through";
-    // bucketList.removeChild(par);
+
   });
 
   par.addEventListener("dblclick", function () {
     bucketList.removeChild(par);
 
-    //remove from local storage
-    localStorage.removeItem(key);
+ //With the help of chat gtp, the inputs of the to-fo lists are converted into keys so they can be removed from local storage.
+    localStorage.removeItem("task_" + par.innerText);
   });
+}
 
